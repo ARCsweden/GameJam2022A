263f8de485dd5f83bc463bbed0a5d5f24c6ed283
William Forsberg <vilfor@hotmail.se>
2022-11-12 15:09:06 +0100

    More Biomes

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -8,10 +8,12 @@ public class MapGeneration : MonoBehaviour
     public GameObject castle;
 
     public int itemsToPlace = 6;
-    public Vector2Int spread = new Vector2Int(20,20);
+    public Vector2Int spread = new Vector2Int(20, 20);
     public float minimunDistance = 10;
 
-    public static int imageScale = 10;
+    public int imageScale = 10;
+    public int blurSize = 10;
+    public int circleSize = 100;
 
     //private Vector2[] castlePos = new Vector2[itemsToPlace];
 
@@ -20,9 +22,11 @@ public class MapGeneration : MonoBehaviour
     {
         Vector2[] castlePos = GenerateCastle();
 
-        Texture2D voroni = GenerateVoroni(castlePos);
+        //Texture2D voroni = GenerateVoroni(castlePos);
 
-        GetComponent<SpriteRenderer>().sprite = Sprite.Create(voroni, new Rect(0.0f, 0.0f, voroni.width, voroni.height), new Vector2(0.5f, 0.5f), imageScale);
+        Texture2D[] biome = GenerateBiome(castlePos);
+
+        GetComponent<SpriteRenderer>().sprite = Sprite.Create(biome[1], new Rect(0.0f, 0.0f, biome[1].width, biome[1].height), new Vector2(0.5f, 0.5f), imageScale);
     }
 
     Vector2[] GenerateCastle()
@@ -43,7 +47,7 @@ public class MapGeneration : MonoBehaviour
             if (distance >= minimunDistance)
             {
                 position.Add(next);
-                Instantiate(castle, new Vector3(next.x,0,next.y), Quaternion.identity);
+                Instantiate(castle, new Vector3(next.x, 0, next.y), Quaternion.identity);
                 toGenerate--;
             }
         }
@@ -51,7 +55,7 @@ public class MapGeneration : MonoBehaviour
         return position.ToArray();
     }
 
-    Texture2D GenerateVoroni(Vector2[] castlePos)
+    /*Texture2D GenerateVoroni(Vector2[] castlePos)
     {
         Vector2Int imageDim = spread * 2 * imageScale;
         Vector2Int[] centroids = new Vector2Int[itemsToPlace];
@@ -62,7 +66,7 @@ public class MapGeneration : MonoBehaviour
             regions[i] = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), 1f);
         }
 
-        Texture2D voroni = new Texture2D(imageDim.x,imageDim.y);
+        Texture2D voroni = new Texture2D(imageDim.x, imageDim.y);
 
         for (int x = 0; x < imageDim.x; x++)
         {
@@ -73,15 +77,60 @@ public class MapGeneration : MonoBehaviour
 
                 for (int i = 0; i < itemsToPlace; i++)
                 {
-                    if(Vector2.Distance((castlePos[i] + spread) * imageScale, new Vector2(x, y)) < distance) { index = i; }
+                    if (Vector2.Distance((castlePos[i] + spread) * imageScale, new Vector2(x, y)) < distance) { index = i; }
                     distance = Mathf.Min(Vector2.Distance((castlePos[i] + spread) * imageScale, new Vector2(x, y)), distance);
                 }
 
-                voroni.SetPixel(x,y,regions[index]);
+                voroni.SetPixel(x, y, regions[index]);
             }
         }
 
         voroni.Apply();
         return voroni;
+    }*/
+
+    Texture2D[] GenerateBiome(Vector2[] castlePos)
+    {
+        LinearBlur blur = new LinearBlur();
+
+        Vector2Int imageDim = spread * 2 * imageScale;
+        Vector2Int[] centroids = new Vector2Int[itemsToPlace];
+
+        Texture2D[] voroni = new Texture2D[itemsToPlace];
+
+        for (int i = 0; i < itemsToPlace; i++)
+        {
+            voroni[i] = new Texture2D(imageDim.x, imageDim.y);
+        }
+
+        for (int x = 0; x < imageDim.x; x++)
+        {
+            for (int y = 0; y < imageDim.y; y++)
+            {
+                float distance = 10000;
+                int index = 0;
+
+                for (int i = 0; i < itemsToPlace; i++)
+                {
+                    voroni[i].SetPixel(x, y, new Color(0f, 0f, 0f, 1f));
+
+                    if (Vector2.Distance((castlePos[i] + spread) * imageScale, new Vector2(x, y)) < distance) { index = i; }
+                    distance = Mathf.Min(Vector2.Distance((castlePos[i] + spread) * imageScale, new Vector2(x, y)), distance);
+                }
+
+                voroni[index].SetPixel(x, y, new Color(circleSize / distance, circleSize / distance, circleSize / distance, 1f));
+            }
+        }
+
+        for (int i = 0; i < itemsToPlace; i++)
+        {
+            voroni[i] = blur.Blur(voroni[i], blurSize, 1);
+
+            voroni[i].Apply();
+        }
+
+        return voroni;
     }
+
+
 }
