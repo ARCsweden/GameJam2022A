25c5f828bc5270b6aa10b7ac3878c9a1f7c4516a
Felix Sjoqvist <felix.sjqv@gmail.com>
2022-11-12 20:34:15 +0100

    added obstacle prefabs

--- Assets/Scripts/HP.cs
+++ Assets/Scripts/HP.cs
@@ -6,17 +6,20 @@ public class HP : MonoBehaviour
 {
     public float HealthPoints = 1;
     public float Armor = 0.75f;
+    public float MaxHealthPoints = 20;
+    private float time_of_last_heal;
+    private float heal_period_time = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(HealthPoints <= 0)
+        if (HealthPoints <= 0)
         {
             Death();
         }
@@ -26,6 +29,23 @@ public class HP : MonoBehaviour
     {
         HealthPoints = HealthPoints - damage * (1 - Armor);
 
+    }
+
+    public void Heal(float heal)
+    {
+        if (Time.time - time_of_last_heal > heal_period_time)
+        {
+            if (HealthPoints + heal <= MaxHealthPoints)
+            {
+                HealthPoints = HealthPoints + heal;
+                Debug.Log("Healing");
+            }
+            else
+            {
+                HealthPoints = MaxHealthPoints;
+            }
+            time_of_last_heal = Time.time;
+        }
     }
 
     public void Death()
